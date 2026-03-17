edec273a7cae309145f417a224e1bbd3ab901b52
Dinesh <bevaradineshsaimanikanta@gmail.com>
2026-03-17 12:54:44 +0530

    remove django_backend (dead code) and untrack sales_platform submodule

--- django_backend/interest/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('detect/', views.DetectInterestView.as_view()),
-    path('mark/', views.MarkInterestView.as_view()),
-    path('respond/', views.RespondInterestView.as_view()),
-]
--- django_backend/trials/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.TrialListCreateView.as_view()),
-    path('stats/', views.TrialStatsView.as_view()),
-    path('<uuid:pk>/', views.TrialDetailView.as_view()),
-]
